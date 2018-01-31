f1b26a046908cfa72f6a48c30aa63749e49a9812
gentilnucleo <gentilnucleo@gentilnucleo-VirtualBox>
2018-01-31 19:23:35 -0300

    AppDataException, falta en capa de datos

--- github.com/gentilcoreac/TPLopapaGentilcore.git/TrabajoPracticoUTN/src/tools/AppDataException.java
+++ github.com/gentilcoreac/TPLopapaGentilcore.git/TrabajoPracticoUTN/src/tools/AppDataException.java
@@ -1,39 +1,40 @@
 package tools;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class AppDataException extends Exception{
 
 	String message;
 	Throwable innerException;
 	
-	
-	
+		
 	public String getMessage() {
 		return message;
 	}
-
-
 
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
-
-
 	public Throwable getInnerException() {
 		return innerException;
 	}
-
-
 
 	public void setInnerException(Throwable innerException) {
 		this.innerException = innerException;
 	}
 
-
-
 	public AppDataException(Throwable e,String message){
 		this.setInnerException(e);
 		this.setMessage(message+":\nAppDataException\n"+e.getMessage());
+	}
+	
+	public AppDataException(Throwable e, String message, Level errorLevel) {
+		this(e,message);
+		Logger logger = LogManager.getLogger(getClass());
+		logger.log(errorLevel,message);
 	}
 	
 }
